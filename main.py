532ca9b0fcc7a387ab6daf51a0fa4e1bc607e5da
Sujay J <sujayj1522002@gmail.com>
2026-09-20 14:56:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def parse_args():
         help="Preview changes without writing files or committing to git",
     )
     parser.add_argument(
-        "--create",
+        "--recreate",
         action="store_true",
         help="Enable verbose logging output",
     )
